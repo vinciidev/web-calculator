eac6c757e89c16503bfede6b5bc6fb489ca27a9a
Pedro Vinicius <vinciidev05@gmail.com>
2024-02-09 12:08:05 -0300

    Finished CSS: Working on JS

---
+++ script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('resultDisplay')
+let currentNumber = ''
+let operator = ''
+let previousNumber = ''
+
+
+function clear() {
+    currentNumber = ''
+    operator = ''
+    previousNumber = ''
+    display.textContent = '0'
+}
+
+function numbers() {
+    let numberZero = document.getElementById('zero')
+    let numberOne = document.getElementById('one')
+    let numberTwo = document.getElementById('two')
+    let numberTree = document.getElementById('tree')
+    let numberFour = document.getElementById('four')
+    let numberFive = document.getElementById('five')
+    let numberSix = document.getElementById('six')
+    let numberSeven = document.getElementById('seven')
+    let numberEight = document.getElementById('eight')
+    let numberNine = document.getElementById('nine')
+
+    
+
+
+}
